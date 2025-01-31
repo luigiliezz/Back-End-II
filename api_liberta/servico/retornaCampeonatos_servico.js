d22ea158387f1d277c8ede824444be4e1f2c0a55
luigiliezz <luizantonioatercio@gmail.com>
2025-01-31 17:45:08 -0400

    oi

---
+++ api_liberta/servico/retornaCampeonatos_servico.js
@@ -0,0 +1,37 @@
+import pool from "./conexao.js";
+
+export async function retornaCampeonatos() {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query('select id, campeao, vice, ano from campeonatos');
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+
+}
+
+export async function retornaCampeonatosID(id) {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query(
+        'SELECT id, campeao, vice, ano FROM campeonatos WHERE id = ' + id);
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
+
+export async function retornaCampeonatosAno(ano) {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query(
+        'SELECT id, campeao, vice, ano FROM campeonatos WHERE ano = + ano');
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
+
+export async function retornaCampeonatosTime(time) {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query(
+        'SELECT id, campeao, vice, ano FROM campeonatos WHERE campeao ="' + time + '"');
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
